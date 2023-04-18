b903f58d733c574d13f91cb2cb5756c8b6621703
Chypre Liang <liang_liuyi@qq.com>
2023-04-18 23:36:50 +0800

    css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"></component>
+  <div class="cp-tabs">
+    <div class="cp-tabs-nav">
+      <div class="cp-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+    </div>
+    <div class="cp-tabs-content">
+      <component class="cp-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+    </div>
   </div>
 </template>
 
@@ -18,7 +22,7 @@ export default {
       }
     });
     const titles = defaults.map((tag) => {
-      return tag.props.title
+      return tag.props.title;
     });
     return {
       defaults,
@@ -28,6 +32,30 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
 
+<style lang="scss">
+$blue: #485fc7;
+$color: #333;
+$border-color: #d9d9d9;
+.cp-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
